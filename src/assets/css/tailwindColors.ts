fee7b7ed65d8b1ab603a912788e46eb46f9ae7e9
Mattia Picchio <picchiomattia@gmail.com>
2024-11-03 17:09:59 +0100

    chore(config): configure Tailwind

---
+++ src/assets/css/tailwindColors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+  gray: {
+    DEFAULT: '#707174',
+    dark: '#5A5A5A',
+    soft: '#CCCCCC',
+  },
+  black: {
+    DEFAULT: '#000',
+    rich: '#040307',
+  },
+  current: 'currentColor',
+  transparent: 'transparent',
+}
