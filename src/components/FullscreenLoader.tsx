4c72484772a15b851fdc80cd4257c837620657af
Mattia Picchio <picchiomattia@gmail.com>
2024-11-15 13:16:23 +0100

    feat: create custom reddit loader

--- src/components/FullscreenLoader.tsx
+++ src/components/FullscreenLoader.tsx
@@ -1,9 +1,9 @@
-import Spinner from '@/components/Spinner'
+import { RedditIcon } from './icons/RedditIcon'
 
 function FullscreenLoader() {
   return (
-    <div className="relative h-screen">
-      <Spinner className="absolute left-1/2 top-1/2 z-50 h-12 w-12" />
+    <div className="relative h-[70vh]">
+      <RedditIcon className="absolute left-1/2 top-1/2 z-50 h-12 w-12 animate-spin" />
     </div>
   )
 }
