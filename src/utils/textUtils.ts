a29858084a8f87728a038f5114df9cb5a6df24d2
Mattia Picchio <picchiomattia@gmail.com>
2024-11-17 12:53:03 +0100

    feat: add generateRandomId utility function

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -1,3 +1,7 @@
 export function isTextNotEmpty(text: string) {
   return Boolean(text && text.toString().trim().length > 0)
 }
+
+export function generateRandomId() {
+  return new Date().getTime()
+}
